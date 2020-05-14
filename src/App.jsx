8384610ec37100dc3d9cf667d46cb7b7a4b4f8b0
Franco <franco.maslup93@hotmail.com>
2020-05-14 02:35:34 -0300

    se movio la logica en App a un componente nuevo llamado FoodList

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,14 @@
 import React, { Component } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import Foods from './Components/Foods';
-import AddFoods from './Components/AddFoods';
+import FoodList from './Components/FoodList';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      foods: [],
-      showFood: false,
-    };
-  }
-
-  componentDidMount() {
-    console.log('component mounted!!!!');
-  }
-
-  componentDidUpdate() {
-    console.log('component updated!');
-  }
-
-  // TO SHOW FOOD
-  toggleFoodHandler = () => {
-    const doesShow = this.state.showFood;
-    this.setState({ showFood: !doesShow });
-  };
-
-  //TO DELETE FOOD
-  deleteFoodHandler = (id) => {
-    const updatedFoods = this.state.foods.filter((food) => food.id !== id);
-    this.setState({ foods: updatedFoods });
-  };
-
-  //TO ADD FOOD
-  addFoodHandler = (foodName) => {
-    const newFood = { name: foodName, id: uuidv4() };
-    const newFoods = [...this.state.foods, newFood];
-    this.setState({
-      foods: newFoods,
-    });
-  };
-
   render() {
-    let foods = null;
-    if (this.state.showFood) {
-      foods = (
-        <div>
-          {this.state.foods.map((foodPito) => {
-            return (
-              <Foods
-                click={() => this.deleteFoodHandler(foodPito.id)}
-                pasta={foodPito.name}
-                key={foodPito.id}
-              />
-            );
-          })}
-        </div>
-      );
-    }
     return (
       <div>
-        <AddFoods addFood={this.addFoodHandler} />
-        <button disabled={this.state.foods.length < 1} onClick={this.toggleFoodHandler}>
-          show food
-        </button>
-        {foods}
+        <FoodList />
       </div>
     );
   }
 }
+
 export default App;
---
+++ src/Components/FoodList.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import Foods from './Foods';
+import AddFoods from './AddFoods';
+
+class FoodList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foods: [],
+      showFood: false,
+    };
+  }
+
+  componentDidMount() {
+    console.log('component mounted!!!!');
+  }
+
+  componentDidUpdate() {
+    console.log('component updated!');
+  }
+
+  // TO SHOW FOOD
+  toggleFoodHandler = () => {
+    const doesShow = this.state.showFood;
+    this.setState({ showFood: !doesShow });
+  };
+
+  //TO DELETE FOOD
+  deleteFoodHandler = (id) => {
+    const updatedFoods = this.state.foods.filter((food) => food.id !== id);
+    this.setState({ foods: updatedFoods });
+  };
+
+  //TO ADD FOOD
+  addFoodHandler = (foodName) => {
+    const newFood = { name: foodName, id: uuidv4() };
+    const newFoods = [...this.state.foods, newFood];
+    this.setState({
+      foods: newFoods,
+    });
+  };
+
+  render() {
+    let foods = null;
+    if (this.state.showFood) {
+      foods = (
+        <div>
+          {this.state.foods.map((foodPito) => {
+            return (
+              <Foods
+                click={() => this.deleteFoodHandler(foodPito.id)}
+                pasta={foodPito.name}
+                key={foodPito.id}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+    return (
+      <div>
+        <AddFoods addFood={this.addFoodHandler} />
+        <button disabled={this.state.foods.length < 1} onClick={this.toggleFoodHandler}>
+          show food
+        </button>
+        {foods}
+      </div>
+    );
+  }
+}
+export default FoodList;
